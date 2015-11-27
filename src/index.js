a88ba905d2bc7642a21fbf11b13164e599f55363
Qian Linfeng <thewawar@gmail.com>
2015-11-27 14:07:50 +0800

    Fix import bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { App } from './App.jsx';
+import { App } from './App';
 
 const records = [
   {color: 'red', size: 'S', style: 'thin', quantity: 1},
