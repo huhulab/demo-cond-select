d63df9feaa439ad40c8f513069926394d6964c12
Qian Linfeng <thewawar@gmail.com>
2015-11-27 15:17:12 +0800

    Reroder records

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import { render } from 'react-dom';
 import { App } from './App';
 
 const records = [
+  /* Red */
   {color: 'red', size: 'S', style: 'thin', quantity: 1},
-  {color: 'blue', size: 'S', style: 'thin', quantity: 8},
-  {color: 'red', size: 'M', style: 'thin', quantity: 1},
-  {color: 'green', size: 'M', style: 'thin', quantity: 11},
-  {color: 'red', size: 'L', style: 'thin', quantity: 3},
   {color: 'red', size: 'S', style: 'fat', quantity: 1},
-  {color: 'blue', size: 'M', style: 'fat', quantity: 2},
+  {color: 'red', size: 'M', style: 'thin', quantity: 1},
+  {color: 'red', size: 'L', style: 'thin', quantity: 3},
   {color: 'red', size: 'L', style: 'fat', quantity: 1},
+  /* Blue */
+  {color: 'blue', size: 'S', style: 'thin', quantity: 8},
+  {color: 'blue', size: 'M', style: 'fat', quantity: 2},
+  /* Green */
+  {color: 'green', size: 'M', style: 'thin', quantity: 11},
+  /* Black */
   {color: 'black', size: 'L', style: 'fat', quantity: 7},
 ]
 
