83866be145bf8b535af1eb563ac840c2fe5ab77b
Qian Linfeng <thewawar@gmail.com>
2015-11-29 00:26:30 +0800

    The prefect version

--- src/App.js
+++ src/App.js
@@ -15,69 +15,117 @@ export const App = React.createClass({
   displayName: 'App',
 
   getInitialState() {
+    const fields = this.props.fields;
+    const records = this.props.records;
     let state = {
-      color: undefined,
-      size: undefined,
-      style: undefined,
-      record: undefined,
-      color_list: {},
-      size_list: {},
-      style_list: {},
+      fields: {},
+      flags: {},
+      records: [...records],
       total: 0,
     };
-    this.props.records.forEach(function(record) {
-      state.color_list[record.color] = true;
-      state.size_list[record.size] = true;
-      state.style_list[record.style] = true;
+    fields.forEach(function(name) {
+      state.fields[name] = undefined;
+      state.flags[name] = {};
+      records.forEach(function(record) {
+        state.flags[name][record[name]] = true;
+      });
+    });
+    records.forEach(function(record) {
       state.total += record.quantity;
     });
     return state;
   },
 
-  updateState(newState) {
+  reset() {
+    let newSelecteds = {}
+    this.props.fields.forEach(function(name) {
+      newSelecteds[name] = undefined;
+    });
+    this.updateState(newSelecteds);
+  },
+
+  getEnabled(records, selectedFields, targetFields, state) {
+    selectedFields.forEach(function(name) {
+      records = records.filter(function(record) {
+        return state[name] === record[name];
+      });
+    });
+    let targetValues = {};
+    targetFields.forEach(function(name) {
+      targetValues[name] = {};
+    });
+    records.map(function(record) {
+      targetFields.forEach(function(name) {
+        targetValues[name][record[name]] = true;
+      });
+    });
+    /* console.log('targetValues:', targetValues); */
+    targetFields.forEach(function(name) {
+      /* console.log(targetValues[name], Object.keys(targetValues[name])); */
+      targetValues[name] = Object.keys(targetValues[name]);
+    });
+    console.log('getEnabled::', selectedFields, targetFields, { records: records, values: targetValues});
+    return { records: records, values: targetValues};
+  },
+
+  updateState(newSelecteds) {
     const self = this;
-    let records = this.props.records;
+    const fields = this.props.fields;
+    const records = this.props.records;
     let localState = {
-      color: this.state.color,
-      size: this.state.size,
-      style: this.state.style,
-      record: undefined,
-      color_list: {},
-      size_list: {},
-      style_list: {},
+      fields: {},
+      flags: {},
+      records: undefined,
       total: 0,
     };
+    fields.forEach(function(name) {
+      localState.fields[name] = self.state.fields[name];
+      localState.flags[name] = self.state.flags[name];
+    });
+
+    Object.keys(newSelecteds).forEach(function(name) {
+      localState.fields[name] = newSelecteds[name];
+    });
 
     records.forEach(function(record) {
-      localState.color_list[record.color] = false;
-      localState.size_list[record.size] = false;
-      localState.style_list[record.style] = false;
+      fields.forEach(function(name) {
+        localState.flags[name][record[name]] = false;
+      });
     });
 
-    Object.keys(newState).forEach(function(name) {
-      localState[name] = newState[name];
+    const selectedFields = fields.filter(function(name) {
+      return localState.fields[name] !== undefined;
     });
-
-    this.props.fields.forEach(function(name) {
-      const value = localState[name]
-      if (value !== undefined) {
-        records = records.filter(function(record) {
-          return record[name] === value;
-        });
-      }
+    const unSelectedFields = fields.filter(function(name) {
+      return localState.fields[name] === undefined;
     });
-
-    localState.record = records.length == 1 ? records[0] : undefined;
-    records.forEach(function(record) {
-      localState.color_list[record.color] = true;
-      localState.size_list[record.size] = true;
-      localState.style_list[record.style] = true;
+    let targetValuesDict = {};
+    let result = this.getEnabled([...records], selectedFields, unSelectedFields, localState.fields);
+    localState.records = result.records;
+    localState.records.forEach(function(record) {
       localState.total += record.quantity;
+    });
+    unSelectedFields.forEach(function(name) {
+      targetValuesDict[name] = result.values[name];
+    });
+
+    selectedFields.forEach(function(name) {
+      let tmpSelectedFields = selectedFields.filter(function(tmpName) {
+        return tmpName !== name;
+      });
+      let result = self.getEnabled([...records], tmpSelectedFields, [name], localState.fields);
+      targetValuesDict[name] = result.values[name];
+    });
+    /* console.log('targetValuesDict:', targetValuesDict); */
+    fields.forEach(function(name) {
+      targetValuesDict[name].forEach(function(value) {
+        localState.flags[name][value] = true;
+      });
     });
     console.log('records:', records);
 
     this.setState(localState);
-    console.log('updateState:', newState, this.state, records);
+    console.log('updateState:', newSelecteds, this.state);
   },
 
   renderGroup(flags, value, onChange) {
@@ -102,38 +150,22 @@ export const App = React.createClass({
     };
 
     /* DOM */
-    let children = [
-      div({style: groupStyle}, this.renderGroup(
-        self.state.color_list, self.state.color, function(e) {
-          self.updateState({
-            color: e.target.value === self.state.color ? undefined : e.target.value
-          });
-          console.log('click color:', e.target.value, self.state);
-        })),
-      div({style: groupStyle}, this.renderGroup(
-        self.state.size_list, self.state.size, function(e) {
-          self.updateState({
-            size: e.target.value === self.state.size ? undefined : e.target.value
-          });
-          console.log('click size:', e.target.value, self.state);
-        })),
-      div({style: groupStyle}, this.renderGroup(
-        self.state.style_list, self.state.style, function(e) {
-          self.updateState({
-            style: e.target.value === self.state.style ? undefined : e.target.value
-          });
-          console.log('click style:', e.target.value,  self.state);
-        })),
+    let children = this.props.fields.map(function(name) {
+      return div({style: groupStyle}, self.renderGroup(
+        self.state.flags[name], self.state.fields[name], function(e) {
+          let newSelecteds = {};
+          newSelecteds[name] = e.target.value === self.state.fields[name] ? undefined : e.target.value
+          self.updateState(newSelecteds);
+          console.log('click ' + name + ':', e.target.value, self.state);
+        }));
+    });
+    children = children.concat([
       ButtonItem({type: 'primary', onClick: function(e) {
-        self.updateState({
-          color: undefined,
-          size: undefined,
-          style: undefined
-        });
+        self.reset();
       }},'Reset'),
-      div({}, "Quantity : ", strong({}, this.state.total))
-    ];
+      div({}, "Quantity : ", strong({}, this.state.total)),
+    ]);
 
-    return div({style: wrapperStyle}, ...children)
+    return div({style: wrapperStyle}, ...children);
   }
 })
